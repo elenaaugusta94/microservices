8e48f77d7bab02afbb44a17ee032da18826e8ce5
elena.augusta <elena.augusta94@gmail.com>
2017-11-02 23:59:56 -0200

    Add Zull

---
+++ Zull/src/main/java/com/zull/ZullApplication.java
@@ -0,0 +1,12 @@
+package com.zull;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZullApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZullApplication.class, args);
+	}
+}
