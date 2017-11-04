995279c15d383d97692a45bccaa7b3d5f50112a7
elena.augusta <elena.augusta94@gmail.com>
2017-11-04 16:01:23 -0200

    Atualização do microsserviço

--- Zull/src/main/java/com/zull/GatewayApplication.java
+++ Zull/src/main/java/com/zull/GatewayApplication.java
@@ -1,12 +1,19 @@
 package com.zull;
 
+import org.apache.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
-public class ZullApplication {
+@EnableZuulProxy
+@EnableEurekaClient
+public class GatewayApplication {
 
+	static Logger logger = Logger.getLogger(GatewayApplication.class);
 	public static void main(String[] args) {
-		SpringApplication.run(ZullApplication.class, args);
+		SpringApplication.run(GatewayApplication.class, args);
+		logger.info("Microserviço ZUUl iniciado com sucesso!");
 	}
 }
