49a7f75db253dfbfbb8041919aaaa9b339889aa3
elena.augusta <elena.augusta94@gmail.com>
2017-11-04 18:01:15 -0200

    Atualização do microsserviço

--- configServer/src/main/java/com/configServer/ConfigServerApplication.java
+++ configServer/src/main/java/com/configServer/ConfigServerApplication.java
@@ -6,8 +6,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+
 @SpringBootApplication
 @EnableConfigServer
+@EnableEurekaClient
 public class ConfigServerApplication {
 	static Logger logger = Logger.getLogger(ConfigServerApplication.class);
 	public static void main(String[] args) {
