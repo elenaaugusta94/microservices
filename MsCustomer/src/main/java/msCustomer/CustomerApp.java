ac893941b4f3784425636b1f791fbac86b540c73
elena.augusta <elena.augusta94@gmail.com>
2017-11-05 16:46:45 -0200

    Atualização do microsservico

--- MsCustomer/src/main/java/msCustomer/CustomerApp.java
+++ MsCustomer/src/main/java/msCustomer/CustomerApp.java
@@ -3,12 +3,14 @@ package msCustomer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @SpringBootApplication(scanBasePackages={"Controller","DAO","Model"})
 @EntityScan("Model")
 @ComponentScan("Controller")
 @EnableJpaRepositories("DAO")
+@EnableEurekaClient
 public class CustomerApp {
 	
 	public static void main(String[] args) {
